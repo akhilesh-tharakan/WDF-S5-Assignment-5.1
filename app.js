15ea24b25d5fc156728c2e1f4e49d9f53d427151
Akhilesh Tharakan <akhilesh.tharakan@gmail.com>
2017-03-19 17:02:54 +0530

    HTML_Javascript upload

---
+++ app.js
@@ -0,0 +1,12 @@
+/* This is the script file from where we are passing the value */	
+/* Session 5 Assignment 1 */
+	
+	var name = "Bryan";
+	var age = 28;
+	var dateofbirth = "14-June-1989";
+	var placeofbirth = "New York";
+	console.log("Details are as below: ");
+	console.log("Name: " + name);
+	console.log("Age: " + age);
+	console.log("Date of Birth: " + dateofbirth);
+	console.log("Place of Birth: " + placeofbirth);
